97c1e9f664750ee5a2411bd11d4b5a732a85dfc3
Diana Magaña <mb15004@ues.edu.sv>
2019-05-21 17:24:42 -0600

    interfaz redi

--- app/src/main/java/ues/ipam135/almacenamiento/AlmacenamientoExterno.java
+++ app/src/main/java/ues/ipam135/almacenamiento/AlmacenamientoExterno.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
